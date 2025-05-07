87e65fbf7e8717ca8ef452021fbc6fcfcf34922a
jenniferjoyce94 <jennifer.joyce.jaglund@gmail.com>
2025-05-07 21:08:08 +0200

    Added price in the API and change the photos. Did the login Modal. Added star as a rating.

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,7 @@
+const openModal = document.querySelector("#open-modal-btn");
+const closeModal = document.querySelector(".close-modal-btn");
+const loginModal = document.querySelector("#login-modal");
+
 const BASE_URL = "http://localhost:1337";
 
 const renderBooks = async () => {
@@ -13,19 +17,66 @@ const renderBooks = async () => {
     let div = document.createElement("div");
     div.className = "book-item";
     div.innerHTML = `
+    ${
+      coverUrl
+        ? `<img src="${BASE_URL}${coverUrl}" alt="${book.title} cover" width="100px" />`
+        : "<p>Ingen bild</p>"
+    }
       <h3 class="book-name">${book.title}</h3>
-      <p class="book-author">Author: ${book.author}</p>
-      <p class="book-length">Length:${book.length} sidor</p>
-      ${
-        coverUrl
-          ? `<img src="${BASE_URL}${coverUrl}" alt="${book.title} cover" width="100px" />`
-          : "<p>Ingen bild</p>"
-      }
-      <pclass="book-release-date">Date of Release: ${book.release_date}</p>
-      <p class="book-rating">Rating: ${book.rating ?? ""}</p>
+      <h4 class="book-author">${book.author}</h4>
+      <p class="book-release-date">Date of Release: ${book.release_date}</p>
+      <p class="book-length">${book.length} pages</p>
+      <p class="book-rating">Rating: ${book.rating ?? ""}
+        <span class="stars" id="stars">
+          <i class="fa-regular fa-star" data-value="1"></i>
+          <i class="fa-regular fa-star" data-value="2"></i>
+          <i class="fa-regular fa-star" data-value="3"></i>
+          <i class="fa-regular fa-star" data-value="4"></i>
+          <i class="fa-regular fa-star" data-value="5"></i>
+        </span>
+      </p>
+      <p class="book-price"> Price: ${book.price} $</p>
       <button class="buy-button">Buy</button>`;
     bookList.appendChild(div);
   });
 };
+
+openModal.addEventListener("click", () => {
+  showModal(loginModal);
+});
+
+closeModal.addEventListener("click", () => {
+  hideModal(loginModal);
+});
+
+function showModal(modal) {
+  modal?.classList.add("show");
+  modal?.classList.remove("hide");
+}
+
+function hideModal(modal) {
+  modal?.classList.remove("show");
+  modal?.classList.add("hide");
+}
+
+async function login() {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  try {
+    let response = await axios.post(`${BASE_URL}/api/auth/local`, {
+      identifier: username,
+      password: password,
+    });
+    const token = response.data.jwt;
+    alert("Login successful!");
+    localStorage.setItem("jwt", token);
+    hideModal(loginModal);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error logging in:", error);
+    alert("Login failed.");
+  }
+}
 
 renderBooks();
