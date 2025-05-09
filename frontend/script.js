026623aa696d5af93d1a48dfe118774e459f02a6
jenniferjoyce94 <jennifer.joyce.jaglund@gmail.com>
2025-05-09 11:23:55 +0200

    added logout function and change the welcome text. Added also the heart function.

--- frontend/script.js
+++ frontend/script.js
@@ -15,6 +15,18 @@ const cancelCreateAccountBtn = document.querySelector(
 
 const BASE_URL = "http://localhost:1337";
 
+function showModal(modal) {
+  modal?.classList.add("show");
+  modal?.classList.remove("hide");
+}
+
+function hideModal(modal) {
+  console.log("event");
+  console.log(modal);
+  modal?.classList.remove("show");
+  modal?.classList.add("hide");
+}
+
 const renderBooks = async () => {
   try {
     let response = await axios.get(`${BASE_URL}/api/books?populate=*`);
@@ -30,11 +42,16 @@ const renderBooks = async () => {
       div.className = "book-item";
       div.dataset.id = book.id;
       div.innerHTML = `
+      <div id="coverHeartContainer">
         ${
           coverUrl
             ? `<img src="${BASE_URL}${coverUrl}" alt="${book.title} cover" width="100px" />`
             : "<p>Ingen bild</p>"
         }
+        <div class="heart-container">
+          <i class="fa-regular fa-heart"></i>
+        </div>
+      </div>
         <h3 class="book-name">${book.title}</h3>
         <h4 class="book-author">${book.author}</h4>
         <p class="book-release-date">Date of Release: ${book.release_date}</p>
@@ -58,59 +75,9 @@ const renderBooks = async () => {
   }
 };
 
-document.addEventListener("click", async (e) => {
-  // Handle star rating
-  if (e.target.classList.contains("fa-star")) {
-    const bookItem = e.target.closest(".book-item");
-    const bookId = bookItem.dataset.id;
-    const rating = e.target.dataset.value;
-
-    const token = localStorage.getItem("jwt");
-    if (!token) {
-      alert("Please log in to rate a book.");
-      return;
-    }
-
-    try {
-      await axios.post(
-        `${BASE_URL}/api/books/${bookId}/ratings`,
-        { rating: parseInt(rating) },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      alert("Rating submitted successfully!");
-    } catch (error) {
-      console.error("Error submitting rating:", error);
-      alert("Failed to submit rating.");
-    }
-  }
-
-  // Handle book purchase
-  if (e.target.classList.contains("buy-button")) {
-    const bookItem = e.target.closest(".book-item");
-    const bookId = bookItem.dataset.id;
-
-    const token = localStorage.getItem("jwt");
-    if (!token) {
-      alert("Please log in to buy a book.");
-      return;
-    }
-
-    try {
-      await axios.post(
-        `${BASE_URL}/api/books/${bookId}/buy`,
-        {},
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      alert("Book purchased successfully!");
-    } catch (error) {
-      console.error("Error purchasing book:", error);
-      alert("Failed to purchase book.");
-    }
-  }
-});
-
 // Show and hide modal
-openModal.addEventListener("click", () => {
+openModal.addEventListener("click", (e) => {
+  e.preventDefault();
   showModal(loginModal);
 });
 
@@ -123,33 +90,15 @@ createAccountBtn.addEventListener("click", (e) => {
   hideModal(loginModal);
   showModal(createAccountModal);
 });
+
 cancelCreateAccountBtn.addEventListener("click", (e) => {
   e.preventDefault();
   hideModal(createAccountModal);
 });
 
-createAccountModal.addEventListener("click", () => {
-  username.value = "";
-  password.value = "";
-  loginText.style.display = "none";
-  loginText.innerHTML = "";
-  hideModal(loginModal);
-  showModal(createAccountModal);
-});
-
 closeCreateAccountModal.addEventListener("click", () => {
   hideModal(createAccountModal);
 });
-
-function showModal(modal) {
-  modal?.classList.add("show");
-  modal?.classList.remove("hide");
-}
-
-function hideModal(modal) {
-  modal?.classList.remove("show");
-  modal?.classList.add("hide");
-}
 
 // Handle login form submission
 loginForm.addEventListener("submit", async (e) => {
@@ -175,8 +124,25 @@ loginForm.addEventListener("submit", async (e) => {
     const token = response.data.jwt;
     alert("Login successful!");
     localStorage.setItem("jwt", token);
-    loginText.innerHTML = `Welcome ${response.data.user.username}`;
+
+    const capitalUsername =
+      response.data.user.username.charAt(0).toUpperCase() +
+      response.data.user.username.slice(1);
+
+    const welcomeMessage = document.querySelector("#welcome-message");
+    welcomeMessage.innerHTML = `Welcome ${capitalUsername}`;
+    welcomeMessage.style.display = "block";
+
+    loginText.innerHTML = `Logout`;
     loginText.style.display = "block";
+    loginText.style.cursor = "pointer";
+    loginText.addEventListener("click", () => {
+      localStorage.removeItem("jwt");
+      loginText.innerHTML = `Login`;
+      loginText.style.cursor = "pointer";
+      alert("Logged out successfully!");
+    });
+
     hideModal(loginModal);
     console.log(response.data);
   } catch (error) {
@@ -225,6 +191,79 @@ createAccountForm.addEventListener("submit", async (e) => {
       alert("Username already exists.");
     } else {
       alert("Failed to create account.");
+    }
+  }
+});
+
+document.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("fa-star")) {
+    const bookItem = e.target.closest(".book-item");
+    const bookId = bookItem.dataset.id;
+    const rating = e.target.dataset.value;
+
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      alert("Please log in to rate a book.");
+      return;
+    }
+
+    try {
+      await axios.post(
+        `${BASE_URL}/api/books/${bookId}/ratings`,
+        { rating: parseInt(rating) },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      alert("Rating submitted successfully!");
+    } catch (error) {
+      console.error("Error submitting rating:", error);
+      alert("Failed to submit rating.");
+    }
+  }
+
+  // Handle book purchase
+  if (e.target.classList.contains("buy-button")) {
+    const bookItem = e.target.closest(".book-item");
+    const bookId = bookItem.dataset.id;
+
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      alert("Please log in to buy a book.");
+      return;
+    }
+
+    try {
+      await axios.post(
+        `${BASE_URL}/api/books/${bookId}/buy`,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      alert("Book purchased successfully!");
+    } catch (error) {
+      console.error("Error purchasing book:", error);
+      alert("Failed to purchase book.");
+    }
+  }
+
+  if (e.target.classList.contains("fa-heart")) {
+    const button = e.target.closest(".fa-heart");
+    const bookItem = button.closest(".book-item");
+
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      alert("Please log in to add a book to favorites.");
+      return;
+    }
+    try {
+      await axios.post(
+        `${BASE_URL}/api/users/me/favorites`,
+        { bookId },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      e.target.classList.add("addedHeart");
+      alert("Book added to favorites!");
+    } catch (error) {
+      console.error("Error adding book to favorites:", error);
+      alert("Failed to add book to favorites.");
     }
   }
 });
